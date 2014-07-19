664711b2de809733c05688137cce2965b708d53d
Sidney Kochman <sidney@kochman.org>
2014-07-18 20:08:58 -0700

    Run Flask server over Tor

--- chatserver.py
+++ chatserver.py
@@ -1,18 +1,8 @@
-import asyncio
 import os
-import socks
 import stem.process
 import tempfile
 
-class ChatServer(asyncio.Protocol):
-
-	def connection_made(self, transport):
-		peername = transport.get_extra_info('peername')
-		print('connection from {}'.format(peername))
-		self.transport = transport
-
-	def data_received(self, data):
-		pass
+from server import app
 
 class ChatHiddenService():
 
@@ -21,25 +11,17 @@ class ChatHiddenService():
 		self.tor_process = stem.process.launch_tor_with_config(
 			config = {
 				'HiddenServiceDir': self.hidden_service_dir.name,
-				'HiddenServicePort': '9999 127.0.0.1:9999'
+				'HiddenServicePort': '80 127.0.0.1:9999'
 			},
 			take_ownership = True
 		)
 		with self.hidden_service_dir as h:
 			with open(os.path.join(h, 'hostname'), 'r') as f:
-				self.onion_address = f.read()
+				self.onion_address = f.read().strip()
 		print('Tor hidden service address: {}'.format(self.onion_address))
 
-	def get_socket():
-		s = socks.socksocket()
-		s.setproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
-		return s
-
-hs = ChatHiddenService()
-s = hs.get_socket()
-
-loop = asyncio.get_event_loop()
-coro = loop.create_server(ChatServer, '127.0.0.1', 9999)
-server = loop.run_until_complete(coro)
-print('Server running on {}'.format(server.sockets[0].getsockname()))
-loop.run_forever()
+if __name__ == '__main__':
+	hs = ChatHiddenService()
+	print('Go to http://{} to chat'.format(hs.onion_address))
+	print()
+	app.run(host='127.0.0.1', port=9999, debug=False)
--- server.py
+++ server.py
@@ -12,4 +12,3 @@ def recive_msg():
 	
 if __name__ == "__main__":
     app.run()
-
